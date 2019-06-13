03f4d2e884605fd3645e004afec0eb794304f329
Daniel Monteiro <danielmntr333@gmail.com>
2019-06-13 23:27:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Navbar from "./components/navbar/navbar";
+import Search from "./components/search/search";
+import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return <AppBar title="Pixabay Image Finder" />;
+  }
+}
+
+export default Navbar;
